f6a17c3f9c26bce0a19fb419aa33f851798b053b
Harish Kumar <harishkumarr193@gmail.com>
2025-05-03 14:50:25 +0530

    Add frontend postcss configuration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { BrowserRouter } from 'react-router'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    {/* /* <BrowserRouter> is used to enable routing in the app */ } 
+    <BrowserRouter> 
     <App />
+    </BrowserRouter>
   </StrictMode>,
+    
 )
